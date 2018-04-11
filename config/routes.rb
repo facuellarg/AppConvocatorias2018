4adeb666632a4da66e4775665a72b40b252f839f
Freddy <facuellarg@unal.edu.co>
2018-04-11 12:40:07 -0500

    first commit(Login)

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
+  post '/users' => 'register#create'
+  devise_for :users
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
