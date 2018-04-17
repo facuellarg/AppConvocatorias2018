5d4bdaf21747c188909fc6f90f5b657e1abd6ae0
juligarji <jgarzonj@unal.edu.co>
2018-04-16 23:17:32 -0500

    modificando migracion de usuario

--- db/migrate/20180411145417_create_users.rb
+++ db/migrate/20180411145417_create_users.rb
@@ -2,9 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.integer :PAPA
-      t.integer :PBM
-
+      t.string :lastname
+      t.string :type
       t.timestamps
     end
   end
