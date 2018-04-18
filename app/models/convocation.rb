4a7395d4f811f6ffdb9e76700eca026c952626a7
juligarji <jgarzonj@unal.edu.co>
2018-04-17 21:16:42 -0500

    finalizando query de buscar convocatorias

--- app/models/convocation.rb
+++ app/models/convocation.rb
@@ -8,9 +8,9 @@ class Convocation < ApplicationRecord
   has_many :student, :through => :applications
   def self.search(search_params)
     
-    return Convocation.joins(:dependences).where("convocations.level = ? AND dependences.id IN (?) AND convocations.payout BETWEEN (?) AND (?) AND convocations.duration BETWEEN (?) AND (?) AND convocations.hours_per_week BETWEEN (?) AND (?)",
-      search_params[:level] || "pregrado",search_params[:dependences] || Dependence.all.pluck(:id),search_params[:payout_min] || Convocation.minimum(:payout),search_params[:payout_max] || Convocation.maximum(:payout),search_params[:hours_per_week_min] || Convocation.minimum(:hours_per_week),
-      search_params[:hours_per_week_max] || Convocation.maximum(:hours_per_week)).order(created_at: :desc)
+    return Convocation.joins(:dependences).where("convocations.level = (?) AND dependences.id IN (?) AND convocations.payout BETWEEN (?) AND (?) AND convocations.duration BETWEEN (?) AND (?) AND convocations.hours_per_week BETWEEN (?) AND (?)",
+      search_params[:level] || "pregrado",search_params[:dependences] || Dependence.all.pluck(:id),search_params[:payout_min] || Convocation.minimum(:payout),search_params[:payout_max] || Convocation.maximum(:payout),search_params[:duration_min] || Convocation.minimum(:duration),
+      search_params[:duration_max] || Convocation.maximum(:duration),search_params[:hours_per_week_min] || Convocation.minimum(:hours_per_week),search_params[:hours_per_week_max] || Convocation.maximum(:hours_per_week)).order(created_at: :desc)
     
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :users
   devise_for :users
   resources :dependences, only: [:index]
+  post 'search_convocations' => 'convocations#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
